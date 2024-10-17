e9ee19abe2d5c04ecb75a26f0f69b4c8d8729320
becastal <bernardo.castaldelli@gmail.com>
2024-10-17 16:33:15 -0300

    origem

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int sair = 0;
+	FILE *arq;
+	
+	while (!sair) {
+		puts("[+] menu inicial.");
+		puts("[0] sair.");
+		puts("[1] criar labiririntos.");
+		puts("[2] resolver labirintos.");
+		puts("[3] resolver caminhos.");
+		puts("[4] jogar MazeMan.");
+
+		printf("\n[?] selecione uma das opcoes: ");
+		int selecao;
+		scanf("%d", &selecao);
+		switch (selecao) {
+			case 0:
+				sair = 1;
+				break;
+			e
+			default:
+				puts("[e] selecao invalida!");
+		}
+	}
+
+	return 0;
+}
