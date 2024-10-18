49351d44ef37ef93cd099d991f09b62cd40fdeab
becastal <bernardo.castaldelli@gmail.com>
2024-10-18 09:57:40 -0300

    origem, incio do projeto

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int sair = 0;
+	
+	while (!sair) {
+		puts("[+] menu inicial.");
+		puts("[0] sair.");
+		puts("[1] (dedalo) criar labiririntos.");
+		puts("[2] (teseu) resolver labirintos.");
+		puts("[3] (minotauro) resolver caminhos.");
+		puts("[4] (pacman) jogar MazeMan.");
+
+		printf("\n[?] selecione uma das opcoes: ");
+		int selecao;
+		scanf("%d", &selecao);
+		switch (selecao) {
+			case 0:
+				sair = 1;
+				break;
+				
+			default:
+				puts("[e] selecao invalida!");
+		}
+	}
+
+	return 0;
+}
